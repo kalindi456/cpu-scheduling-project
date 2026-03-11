9ed8b38753e759fc78a91a06b2c9a7de7e748b23
kalindi456 <kalindipareek.kp@gmail.com>
2026-03-11 16:40:32 +0000

    added fcfs.c

---
+++ fcfs.c
@@ -0,0 +1,28 @@
+#include "../include/scheduler.h"
+
+void fcfs(struct process p[], int n)
+{
+    float time = 0;
+    float total_tat = 0;
+    float total_wt = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(time < p[i].at)
+        {
+            time = p[i].at;
+        }
+
+        time += p[i].bt;
+
+        p[i].ct = time;
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
+
+        total_tat += p[i].tat;
+        total_wt += p[i].wt;
+    }
+
+    float avg_tat = total_tat / n;
+    float avg_wt = total_wt / n;
+}
